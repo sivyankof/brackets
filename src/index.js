66558132f181d222fd5187838821f36ac972c926
Artem Sivyankov <morfiy0393@gmail.com>
2021-02-05 11:44:10 +0300

    init task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arr = bracketsConfig.map((e) => e.join(""));
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (str.includes(arr[i])) {
+            str = str.replace(arr[i], "");
+            i = -1;
+        }
+
+    }
+    return str.length === 0;
+};
